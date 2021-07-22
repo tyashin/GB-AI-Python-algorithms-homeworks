05e1a8751acac054383d3b8bbac19ee86b87e789
tyashin <tyashin@gmail.com>
2021-07-22 05:19:40 +0300

    Assignment-5 completed

--- lesson-5/les_5_task_1.py
+++ lesson-5/les_5_task_1.py
@@ -1,9 +1,44 @@
 '''
-1. Пользователь вводит данные о количестве предприятий, их наименования и прибыль 
-за четыре квартала для каждого предприятия. Программа должна определить 
-среднюю прибыль (за год для всех предприятий) и отдельно вывести наименования предприятий, 
+1. Пользователь вводит данные о количестве предприятий, их наименования и прибыль
+за четыре квартала для каждого предприятия. Программа должна определить
+среднюю прибыль (за год для всех предприятий) и отдельно вывести наименования предприятий,
 чья прибыль выше среднего и ниже среднего.
 
 Задача считается решённой, если в ней использована как минимум одна коллекция из модуля collections.
 
 '''
+
+import re
+from collections import namedtuple
+from statistics import mean
+from sys import exit
+
+Enterprise = namedtuple(
+    'Enterprise', ['title', 'q1', 'q2', 'q3', 'q4', 'profit'])
+regex = '^[-]?[0-9]*[,.]?[0-9]*$'
+ent_num = int(input('Введите количество предприятий: '))
+ent_list = []
+
+for i in range(ent_num):
+    ent_info = input(
+        'Введите через пробел название предприятия и значения прибыли за каждый из 4-х кварталов: ').split()
+
+    if len(ent_info) != 5 or len([i for i in ent_info if re.search(regex, i)]) != 4:
+        print('Ошибка ввода.')
+        exit()
+
+    ent_list.append(Enterprise(*ent_info, sum(
+        float(j) for j in ent_info[1:])))
+
+avg_total = mean([i.profit for i in ent_list])
+print(f'Средняя прибыль по всем предприятиям: {avg_total}.')
+
+for i in ent_list:
+    print(
+        f'Предприятие: {i.title}. Прибыль за четыре квартала: {i.profit}.')
+
+for i in ent_list:
+    if i.profit > avg_total:
+        print(f'Прибыль предприятия {i.title} выше средней.')
+    elif i.profit < avg_total:
+        print(f'Прибыль предприятия {i.title} ниже средней.')
--- lesson-5/les_5_task_2.py
+++ lesson-5/les_5_task_2.py
@@ -1,10 +1,111 @@
 '''
-2. Написать программу сложения и умножения двух шестнадцатеричных чисел. 
+2. Написать программу сложения и умножения двух шестнадцатеричных чисел.
 При этом каждое число представляется как массив, элементы которого — цифры числа.
-Например, пользователь ввёл A2 и C4F. Нужно сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно. 
+Например, пользователь ввёл A2 и C4F. Нужно сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
 Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
-Примечание: Если воспользоваться функциями hex() и/или int() для преобразования систем счисления, 
-задача решается в несколько строк. Для прокачки алгоритмического мышления такой вариант не подходит. 
+Примечание: Если воспользоваться функциями hex() и/или int() для преобразования систем счисления,
+задача решается в несколько строк. Для прокачки алгоритмического мышления такой вариант не подходит.
 Поэтому использование встроенных функций для перевода из одной системы счисления в другую в данной задаче под запретом.
+Вспомните начальную школу и попробуйте написать сложение и умножение в столбик.
+
+Задача считается решённой, если в ней использована как минимум одна коллекция из модуля collections.
 
 '''
+
+from collections import defaultdict, deque
+
+
+class Hex_arithmetic():
+
+    def __init__(self, num1, num2):
+        self.hex_digits = [str(i) for i in range(10)] + \
+            ['A', 'B', 'C', 'D', 'E', 'F']
+
+        self.num1 = num1.upper()
+        self.num2 = num2.upper()
+
+        if len(self.num1) < len(self.num2):
+            self.num1, self.num2 = self.num2, self.num1
+
+    def add_hex_nums(self, n1=None, n2=None):
+        carry_on = 0
+        sum = []
+
+        if n1 == n2 == None:
+            n1 = self.num1[:][::-1]
+            n2 = self.num2[:][::-1]
+        else:
+            n1 = n1[::-1]
+            n2 = n2[::-1]
+            if len(n1) < len(n2):
+                n1, n2 = n2, n1
+
+        for i in range(len(n1)):
+            digit = self.hex_digits.index(
+                n1[i]) + (self.hex_digits.index(n2[i]) if i < len(n2) else 0) + carry_on
+
+            if digit > 15:
+                digit = digit - 16
+                carry_on = 1
+            else:
+                carry_on = 0
+
+            sum.append(self.hex_digits[digit])
+
+        if carry_on == 1:
+            sum.append(self.hex_digits[1])
+
+        return sum[::-1]
+
+    def multiply_hex_nums(self):
+        carry_on = 0
+        num_dict = defaultdict(int)
+        k = 0
+        n1 = self.num1[:][::-1]
+        n2 = self.num2[:][::-1]
+
+        for i in range(len(n2)):
+            num_list = deque()
+
+            for _ in range(i):
+                num_list.appendleft("0")
+
+            for j in range(len(n1)):
+
+                digit = self.hex_digits.index(n1[j]) * \
+                    self.hex_digits.index(n2[i]) + carry_on
+
+                if digit > 15:
+                    spam = digit % 16
+                    carry_on = (digit - spam)//16
+                    digit = spam
+
+                else:
+                    carry_on = 0
+
+                num_list.append(self.hex_digits[digit])
+
+            while carry_on > 0:
+                if carry_on > 15:
+                    digit = carry_on % 16
+                    carry_on = carry_on - 16
+                    num_list.append(self.hex_digits[digit])
+                else:
+                    num_list.append(self.hex_digits[carry_on])
+                    carry_on = 0
+
+            num_dict[k] = num_list
+            k += 1
+
+        product = ['0']
+        for i in num_dict.values():
+            product = self.add_hex_nums(product, list(i)[::-1])
+
+        return product
+
+
+num1 = input('Введите первое шестнадцатеричное число:')
+num2 = input('Введите второе шестнадцатеричное число:')
+ha = Hex_arithmetic(num1, num2)
+print(f'{ha.num1} + {ha.num2} = {ha.add_hex_nums()}')
+print(f'{ha.num1} * {ha.num2} = {ha.multiply_hex_nums()}')
